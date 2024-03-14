4df111b692b4e7567d88b298d87d6424aa97af30
Irfan <irfan188iqbal@gmail.com>
2024-03-14 21:55:56 +0530

    pratice node js

---
+++ Node js/stream.js
@@ -0,0 +1,10 @@
+const fs=require('node:fs')
+
+const readableStream=fs.createReadStream('./file.txt',{encoding:'utf-8',highWaterMark:2})
+
+const writablestream=fs.createWriteStream('./file2.txt')
+
+readableStream.on('data',(chunk)=>{
+    console.log(chunk)
+    writablestream.write(chunk)
+})
--- Reflect.js
+++ Reflect.js
@@ -3,6 +3,7 @@ let obj={
     age:33
 }
 
+
 console.log(Reflect.get(obj,"name"))  //retrive the value of the obj
 Reflect.set(obj,"place","puthiyangadi")  //set value or modify the value
 console.log(obj)
